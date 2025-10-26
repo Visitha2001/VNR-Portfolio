552daa840a0d99defc6ec099722a6100b5633345
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-10-26 13:58:42 +0530

    header done

---
+++ components/DropDowm.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Menu } from "lucide-react";
+import Link from "next/link"
+
+export function DropDowm({navLinks}: {navLinks: {href: string; label: string}[]}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="px-4 py-2 dark bg-gray-800/50 backdrop-blur-sm rounded-full text-white hover:text-blue-400 transition-all">
+            <Menu className="w-10 h-10" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-auto dark bg-gray-800/50 backdrop-blur-sm rounded-lg" align="start">
+        <DropdownMenuLabel>Visitha N. Rajapaksha</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+            {navLinks.map((link) => (
+              <DropdownMenuItem key={link.label}>
+                <Link href={link.href} className="text-white hover:text-blue-300 transition-all cursor-pointer">{link.label}</Link>
+              </DropdownMenuItem>
+            ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image"
+import Link from "next/link"
+import { DropDowm } from "./DropDowm"
+import { Button } from "@/components/ui/button"
+
+const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/projects", label: "Projects" },
+  { href: "/contact", label: "Contact" },
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-transparent m-2 sticky top-2">
+      <div className="container mx-auto flex items-center justify-between px-6 bg-gray-900/50 backdrop-blur-sm rounded-full">
+        <div className="flex items-center w-auto h-20 object-contain">
+          <Link href="/" className="text-white hover:text-blue-400 transition-all cursor-pointer">
+            <Image src="/no-bg.png" alt="Logo" width={150} height={100} />
+          </Link>
+        </div>
+        <div className="hidden sm:block"> 
+          <ul className="flex items-center gap-5 font-medium">
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <Link href={link.href} className="text-white hover:text-blue-300 transition-all cursor-pointer">{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="hidden lg:block">
+          <Button variant="outline" className="px-4 py-2 bg-white hover:bg-blue-400 border-0 text-black rounded-full transition-all" >
+            Get in touch
+          </Button>
+        </div>
+        <div className="block sm:hidden">
+          <DropDowm navLinks={navLinks} />
+        </div>
+      </div>
+    </header>
   )
 }
 
